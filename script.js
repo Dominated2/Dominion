d172da0bc1b99b9a0640ab630a8f66d4074bed3b
Dominated2 <118030865+Dominated2@users.noreply.github.com>
2025-11-08 15:48:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let age = 60;
+if (age < 18) {
+  console.log("Minor");
+} else {
+  console.log("Adult");
+}
+
+let num = 5;
+if (num > 0) {
+  console.log("Positive");
+} else {
+  console.log("Not positive");
+}
+
+let x = 10, y = 10;
+if (x == y) {
+  console.log("Equal values");
+} else {
+  console.log("Not equal");
+}
+
+let a = 5, b = "5";
+if (a === b) {
+  console.log("Strictly equal");
+} else {
+  console.log("Not strictly equal");
+}
+
+let online = false;
+if (!online) {
+  console.log("User is offline");
+} else {
+  console.log("User is online");
+}
